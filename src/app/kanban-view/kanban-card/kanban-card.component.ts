caac117336d1c1640da86a7290a335af84d7291d
Deepak More <deepakmore@outlook.com>
2021-11-16 11:21:47 +0530

    Kanban Stage and Card created

--- src/app/kanban-view/kanban-card/kanban-card.component.ts
+++ src/app/kanban-view/kanban-card/kanban-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-kanban-card',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanban-card.component.css']
 })
 export class KanbanCardComponent implements OnInit {
+  @Input() card: object;
+  @Output() onCardSelected: EventEmitter<Object> = new EventEmitter<Object>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCardSelect(id) {
+    this.onCardSelected.emit(id);
+  }
 }
--- src/app/kanban-view/kanban-stage/kanban-stage.component.ts
+++ src/app/kanban-view/kanban-stage/kanban-stage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-kanban-stage',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KanbanStageComponent implements OnInit {
 
-  constructor() { }
+  @Input() stage;
+  @Output() onCardselect: EventEmitter<Object> = new EventEmitter<Object>();
+
+  constructor() {}
 
   ngOnInit() {
   }
 
+  onCardSelected(data) {
+    this.onCardselect.emit(data);
+  }
 }
